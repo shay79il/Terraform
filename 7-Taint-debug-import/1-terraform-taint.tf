b1c6381a3a6ec3b0258abc91e8c2c15494c6ff6e
Shay Goldstein <shay79il@gmail.com>
2022-05-23 17:20:52 +0300

    update when to use taint and untaint commands

--- 7-Taint-debug-import/1-terraform-taint.tf
+++ 7-Taint-debug-import/1-terraform-taint.tf
@@ -1,5 +1,22 @@
 
 
+
+# Using taint command is for situations when a manual change has occure 
+# lets say a version of nginx over the VM running it we could run 'terraform destroy' 
+# and 'terraform apply' command after
+# BUT
+# Instead we could 'taint' the resource by using the 'terraform taint'  command 
+# and run 'terraform apply' command after this way ONLY the relevant resource will be 
+# recreated 
+
+# On the contrary
+# When we run an 'apply' command and it failed ONLY due to 
+# something we dont have problem with (for example the 
+# following resource block which just 'cat' the ip to a local file)
+# but the "aws_instance" provisioned successfully we can run 
+# the 'terraform untaint' command to the relevent tainted resource
+
+
 resource "aws_instance" "webserver-3" {
   ami           = "ami-0edab43b6fa892279"
   instance_type = "t2.micro"
