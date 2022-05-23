065d15dadf6610db540d5b39255463653e41d6cb
Shay Goldstein <shay79il@gmail.com>
2022-05-23 14:24:40 +0300

    data source refering

--- 3-Working-with-terraform/3-data-sources.tf
+++ 3-Working-with-terraform/3-data-sources.tf
@@ -21,3 +21,28 @@ resource "local_file" "pet" {
   filename = "/root/pets.txt"
   content  = data.local_file.pet.content
 }
+
+# Another example
+#################
+
+data "aws_key_pair" "alpha" {
+  
+  # We can use only this 
+  # in the DATA block to identify
+  # the UNMANAGED terraform resource
+  key_name = "alpha"
+  
+  
+  #Or 
+  # we can use the 'filter' attribute 
+  filter {
+    name = "tag:project"
+    values = ["cerberus"]
+  }
+
+}
+resource "aws_instance" "cerberus" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = data.aws_key_pair.alpha.key_name
+}
