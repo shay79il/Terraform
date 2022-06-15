4d5c2fb3a745575f59484467e4112e14ba27298e
Shay Goldstein <s.goldstein@draftkings.com>
2022-06-15 13:33:51 +0300

    use of json file

--- 4-Terraform-with-AWS/3-S3.tf
+++ 4-Terraform-with-AWS/3-S3.tf
@@ -18,21 +18,26 @@ data "aws_iam_group" "finance-data" {
 
 resource "aws_s3_bucket_policy" "finance-policy" {
   bucket = aws_s3_bucket.finance.id
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : "*",
-          "Effect" : "Allow",
-          "Resource" : "arn:aws:s3:::${aws_s3_bucket.finance.id}/*",
-          "Principal" : {
-            "AWS" : [
-              "${data.aws_iam_group.finance-data.arn}"
-            ]
-          }
-        }
-      ]
-    }
-  )
+  
+  # Option 1 - use of a json file
+  policy = file("policies/s3-policy.json")
+
+  # Option 2 - use of a json file
+  # policy = jsonencode(
+  #   {
+  #     "Version" : "2012-10-17",
+  #     "Statement" : [
+  #       {
+  #         "Action" : "*",
+  #         "Effect" : "Allow",
+  #         "Resource" : "arn:aws:s3:::${aws_s3_bucket.finance.id}/*",
+  #         "Principal" : {
+  #           "AWS" : [
+  #             "${data.aws_iam_group.finance-data.arn}"
+  #           ]
+  #         }
+  #       }
+  #     ]
+  #   }
+  # )
 }
